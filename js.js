8f65ab5a0c8d110761b484518ffa7daeda202bc0
SummerJyl <137237074+SummerJyl@users.noreply.github.com>
2025-07-04 20:26:40 -0700

    Update site for GitHub Pages

--- js.js
+++ js.js
@@ -11,83 +11,71 @@ function toggleMenu() {
 const videoPlay = document.querySelectorAll(".video-thumbnail");
 videoPlay.forEach((btn) => {
     btn.addEventListener("click", function () {
-        this/classList.add("d-none");
+        this.classList.add("d-none");
         const videoCurrent = this.previousElementSibling;
         videoCurrent.play();
-        videoCurrent.control = true;
+        videoCurrent.controls = true;
 
-        // When video ends remove show controls and show video thumbnail
+        // When video ends remove controls and show video thumbnail
         videoCurrent.addEventListener("ended", function () {
-            videoCurrent.control = false;
-            this.nextElementSibling.classList.remove("d-none");
+            videoCurrent.controls = false;
+            btn.classList.remove("d-none");
         });
-    })
-})
+    });
+});
 // End Play video on click
 
-// Swiper slider 1
-    var swiper = new Swiper(".challenge-slider", {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      // autoplay: {
-      //    delay: 2500,
-      //    disableOnInteraction: false,
-      // },
-      pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-      },
-      navigation: {
-        nextEl: ".challenge-slider .swiper-button-next",
-        prevEl: ".challenge-slider .swiper-button-prev",
-      },
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-      },
-    });
-// End Swiper slider 1
-
-// Swiper slider 2
-    var swiper = new Swiper(".course-slider", {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      // autoplay: {
-      //    delay: 2500,
-      //    disableOnInteraction: false,
-      // },
-  navigation: {
-        nextEl: ".course-slider .swiper-button-next",
-        prevEl: ".course-slider .swiper-button-prev",
-  },
-  breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        1170: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-        },
-      },
-  });
-// End Swiper slider 2
-
-// Swiper slider 3
-var swiper = new Swiper(".course-slider", {
+// Swiper slider 1 - Challenge Slider
+var challengeSwiper = new Swiper(".challenge-slider", {
   slidesPerView: 1,
   spaceBetween: 30,
-  // autoplay: {
-  //    delay: 2500,
-  //    disableOnInteraction: false,
-  // },
-navigation: {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".challenge-slider .swiper-button-next",
+    prevEl: ".challenge-slider .swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
+});
+// End Swiper slider 1
+
+// Swiper slider 2 - Course Slider
+var courseSwiper = new Swiper(".course-slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
     nextEl: ".course-slider .swiper-button-next",
     prevEl: ".course-slider .swiper-button-prev",
-},
-breakpoints: {
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1170: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
+// End Swiper slider 2
+
+// Swiper slider 3 - Teacher Slider (example class)
+var teacherSwiper = new Swiper(".teacher-slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".teacher-slider .swiper-button-next",
+    prevEl: ".teacher-slider .swiper-button-prev",
+  },
+  breakpoints: {
     640: {
       slidesPerView: 2,
       spaceBetween: 30,
